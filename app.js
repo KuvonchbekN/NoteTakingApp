9b1d7d330848d4974f9162b2bf5244861e92edd1
KuvonchbekN <knormakhmatov@students.wiut.uz>
2022-04-02 07:36:44 +0500

    added note card and some css

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
 
+app.set("view engine", "pug"); //this will set the view engine to the pug
+app.use("/static", express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log("The server is working properly!");
+});
